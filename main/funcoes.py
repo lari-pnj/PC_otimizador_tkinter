29ffa7aad360642ad6d5e215becca1d97cb757eb
Larissa valleriano <larissavaleriano36@gmail.com>
2025-09-10 14:29:16 -0300

    adicionei o arquivo para funcoes

---
+++ main/funcoes.py
@@ -0,0 +1,50 @@
+import os
+import subprocess
+import psutil
+import shutil
+
+
+def desativar_recursos():
+    print('recurso não adicionado')
+
+
+
+def desinstalar_app():
+     print('recurso não adicionado')
+
+
+
+def atualizar_drives():
+     print('recurso não adicionado')
+
+
+
+def excluir_arquivos():
+     print('recurso não adicionado')
+
+
+
+def recursos_energia():
+     print('recurso não adicionado')
+
+
+
+def limpar_cache():
+     print('recurso não adicionado')
+
+
+def desfragmentar_disco():
+     print('recurso não adicionado')
+
+
+def configuracoes_visuais():
+     print('recurso não adicionado')
+
+
+def limpar_prefetch_temp():
+     print('recurso não adicionado')
+    
+
+def apps_inicializacao(): 
+     print('recurso não adicionado')   
+
