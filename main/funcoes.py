e42db1b9f5551ad5275957ca03a6719ada87d6b2
Larissa valleriano <larissavaleriano36@gmail.com>
2025-09-11 19:12:49 -0300

    adicionei algumas funções

--- main/funcoes.py
+++ main/funcoes.py
@@ -3,14 +3,19 @@ import subprocess
 import psutil
 import shutil
 
-
+#Por que desativar recursos?
+# Melhorar o desempenho: 
+# Desativar recursos desnecessários pode tornar o sistema mais leve e rápido, 
+# ao impedir o carregamento de programas e serviços que você não usa. 
 def desativar_recursos():
-    print('recurso não adicionado')
+     subprocess.run('OpitionalFeature.exe', shell=True)
+     print('recurso não adicionado')
 
 
 
 def desinstalar_app():
-     print('recurso não adicionado')
+     subprocess.run('appwiz.cpl', shell = True)
+     
 
 
 
@@ -34,7 +39,8 @@ def limpar_cache():
 
 
 def desfragmentar_disco():
-     print('recurso não adicionado')
+     subprocess.run('cleanmgr', shell = True)
+     
 
 
 def configuracoes_visuais():
@@ -48,3 +54,22 @@ def limpar_prefetch_temp():
 def apps_inicializacao(): 
      print('recurso não adicionado')   
 
+
+
+#Recursos futuros------------------------------------
+
+def monitorar_temperatura():
+     subprocess.run("resmon.exe", shell=True)
+     
+     
+#Liberar cache do DNS (pode acelerar internet às vezes)  
+def limpar_cache_dns():
+     subprocess.run('ipconfig /flushdns', shell = True) 
+     
+resultado = subprocess.run("ipconfig /flushdns", shell=True, capture_output=True, text=True)
+
+print("Saída:")
+print(resultado.stdout)
+print("Erros:")
+print(resultado.stderr)  
+    
